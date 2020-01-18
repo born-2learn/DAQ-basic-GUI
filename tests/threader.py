302e7b639694ffb4183239c30797e4bd9d26ba4f
Syed Farhan <42010556+born-2learn@users.noreply.github.com>
2020-01-18 17:38:48 +0530

    Added threader.py

--- tests/threader.py
+++ tests/threader.py
@@ -4,13 +4,193 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 import serial
+from PIL import Image, ImageTk
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 serial_data = ''
 filter_data = ''
 update_period = 5
 serial_object = None
-gui = Tk()
-gui.title("Citriot DAQ")
+main = Tk()
+main.title("Citriot DAQ")
+
+
+def toggledo1():
+    if do1.config('text')[-1] == 'ON':
+        do1.config(text='OFF')
+        GPIO.output(25, GPIO.LOW)
+    else:
+        do1.config(text='ON')
+        GPIO.output(25, GPIO.HIGH)
+
+
+def toggledo2():
+    if do2.config('text')[-1] == 'ON':
+        do2.config(text='OFF')
+        GPIO.output(17, GPIO.LOW)
+
+    else:
+        do2.config(text='ON')
+        GPIO.output(17, GPIO.HIGH)
+
+
+def toggledo3():
+    if do3.config('text')[-1] == 'ON':
+        do3.config(text='OFF')
+        GPIO.output(18, GPIO.LOW)
+    else:
+        do3.config(text='ON')
+        GPIO.output(18, GPIO.HIGH)
+
+
+def toggledo4():
+    if do4.config('text')[-1] == 'ON':
+        do4.config(text='OFF')
+        GPIO.output(27, GPIO.LOW)
+
+    else:
+        do4.config(text='ON')
+        GPIO.output(27, GPIO.HIGH)
+
+
+def toggledo5():
+    if do5.config('text')[-1] == 'ON':
+        do5.config(text='OFF')
+        GPIO.output(22, GPIO.LOW)
+    else:
+        do5.config(text='ON')
+        GPIO.output(22, GPIO.HIGH)
+
+
+def toggledo6():
+    if do6.config('text')[-1] == 'ON':
+        do6.config(text='OFF')
+        GPIO.output(23, GPIO.LOW)
+    else:
+        do6.config(text='ON')
+        GPIO.output(23, GPIO.HIGH)
+
+
+def toggledo7():
+    if do7.config('text')[-1] == 'ON':
+        do7.config(text='OFF')
+        GPIO.output(24, GPIO.LOW)
+    else:
+        do7.config(text='ON')
+        GPIO.output(24, GPIO.HIGH)
+
+
+def toggledo8():
+    if do8.config('text')[-1] == 'ON':
+        do8.config(text='OFF')
+        GPIO.output(10, GPIO.LOW)
+    else:
+        do8.config(text='ON')
+        GPIO.output(10, GPIO.HIGH)
+
+# gives weight to the cells in the grid
+rows = 0
+while rows < 50:
+    main.rowconfigure(rows, weight=1)
+    main.columnconfigure(rows, weight=1)
+    rows += 1
+
+# Defines and places the notebook widget
+nb = ttk.Notebook(main)
+nb.grid(row=1, column=0, columnspan=50, rowspan=49, sticky='NESW')
+
+
+# About
+aboutPage = ttk.Frame(nb)
+nb.add(aboutPage, text='About')
+#image = PhotoImage(file="logo.gif")
+#L = Label(aboutPage, image=image).pack()
+
+image = Image.open("logo.jpg")
+image = image.resize((500, 250), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+img = ImageTk.PhotoImage(image)
+panel = Label(aboutPage, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+Label(aboutPage, text = "Citriot Data Acquisition System",font=("Helvetica", 24) ).pack()
+
+doPage = ttk.Frame(nb)
+nb.add(doPage, text='Digital Output')
+
+Label(doPage, text="DO 1").grid(column=0,row=1,padx=10,pady=10)
+do1 = Button(doPage, text="OFF", width=12, command=toggledo1)
+do1.grid(row=1, column = 1)
+
+Label(doPage, text="DO 2").grid(column=0,row=2,padx=10,pady=10)
+do2 = Button(doPage, text="OFF", width=12, command=toggledo2)
+do2.grid(row=2, column = 1)
+
+Label(doPage, text="DO 3").grid(column=0,row=3,padx=10,pady=10)
+do3 = Button(doPage, text="OFF", width=12, command=toggledo3)
+do3.grid(row=3, column = 1)
+
+Label(doPage, text="DO 4").grid(column=0,row=4,padx=10,pady=10)
+do4 = Button(doPage, text="OFF", width=12, command=toggledo4)
+do4.grid(row=4, column = 1)
+
+Label(doPage, text="DO 5").grid(column=0,row=5,padx=10,pady=10)
+do5 = Button(doPage, text="OFF", width=12, command=toggledo5)
+do5.grid(row=5, column = 1)
+
+Label(doPage, text="DO 6").grid(column=0,row=6,padx=10,pady=10)
+do6 = Button(doPage, text="OFF", width=12, command=toggledo6)
+do6.grid(row=6, column = 1)
+
+Label(doPage, text="DO 7").grid(column=0,row=7,padx=10,pady=10)
+do7 = Button(doPage, text="OFF", width=12, command=toggledo7)
+do7.grid(row=7, column = 1)
+
+Label(doPage, text="DO 8").grid(column=0,row=8,padx=10,pady=10)
+do8 = Button(doPage, text="OFF", width=12, command=toggledo8)
+do8.grid(row=8, column = 1)
+
+
+
+
+
+
+
+
+
+# DI
+
+diPage = ttk.Frame(nb)
+nb.add(diPage, text='Digital Input')
+
+
+
+
+
+
+
+
+# Thermocouple
+thermocouplePage = ttk.Frame(nb)
+nb.add(thermocouplePage, text='Thermocouple')
+#nb.Label(main, text="Citriot Data Acquisition System").grid(row=5)
+
+
+
+
+
+
+
+
+
+
+# Analog Input
+analogPage = ttk.Frame(nb)
+nb.add(analogPage, text='Analog Input')
+
+
+# DO
+doPage = ttk.Frame(nb)
+nb.add(doPage, text='Digital Output')
 
 
 def connect():
@@ -62,7 +242,7 @@ def get_data():
             pass
 
 
-def update_gui():
+def update_main():
     """" This function is an update function which is also threaded. The function assimilates the data
     and applies it to it corresponding progress bar. The text box is also updated every couple of seconds.
     A simple auto refresh function .after() could have been used, this has been avoid purposely due to
@@ -156,27 +336,13 @@ def update_gui():
                 new = time.time()
 
 
-def send():
-    """This function is for sending data from the computer to the host controller.
-
-        The value entered in the the entry box is pushed to the UART. The data can be of any format, since
-        the data is always converted into ASCII, the receiving device has to convert the data into the required f
-        format.
-    """
-
-
-    if not send_data:
-        print("Sent Nothing")
-
-    serial_object.write(send_data)
-
 
 def disconnect():
     """
     This function is for disconnecting and quitting the application.
     Sometimes the application throws a couple of errors while it is being shut down, the fix isn't out yet
     but will be pushed to the repo once done.
-    simple GUI.quit() calls.
+    simple main.quit() calls.
     """
     try:
         serial_object.close()
@@ -184,12 +350,12 @@ def disconnect():
     except AttributeError:
         print("Closed without Using it -_-")
 
-    gui.quit()
+    main.quit()
 
 
 if __name__ == "__main__":
     """
-    The main loop consists of all the GUI objects and its placement.
+    The main loop consists of all the main objects and its placement.
     The Main loop handles all the widget placements.
     """
     global var
@@ -199,7 +365,7 @@ if __name__ == "__main__":
     text = Text(width=30, height=1)
 
     # threads
-    t2 = threading.Thread(target=update_gui)
+    t2 = threading.Thread(target=update_main)
     t2.daemon = True
     t2.start()
 
@@ -257,5 +423,5 @@ if __name__ == "__main__":
     disconnect = Button(text="Disconnect", command=disconnect).place(x=300, y=360)
 
     # mainloop
-    gui.geometry('500x500')
-    gui.mainloop()
+    main.geometry('500x500')
+    main.mainloop()
